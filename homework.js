a45778486342d1fc7834c4fc911161d56650de14
Giovannyf99 <giovannyf1999@gmail.com>
2023-03-09 09:25:07 -0500

    mostly done

---
+++ homework.js
@@ -0,0 +1,93 @@
+const prompt = require('prompt-sync')({ sigint: true });
+let work = prompt
+
+// Work or Sleep In
+// Write a script that asks for a day of the week. If the day is a business day, print 'go to work!'. If the day is a weekend day, print 'sleep in!'. If whatever the user entered is not a day, print 'enter a valid day'.
+
+// const day = prompt('what day is it?')
+// if(day == 'saturday' || day == 'sunday'){
+//     console.log('sleep in!')
+// } else {
+//     console.log('go to work!')
+// }
+
+// Secret Password
+// Write a script that asks the user for a password. If the user enters the correct word (pick one yourself) then print 'Welcome!', otherwise print 'Try Again!'
+
+// const password = prompt("What is your password?")
+//     if (password = 'yes'){
+//         console.log('Welcome!')
+//     } else{
+//         console.log('Try Again!')
+//     }
+
+
+// Day of Week
+// Write a script that asks the user for a number between 1-7. Print the corresponding day for the number, (i.e. 1 = 'Sunday', 2 = 'Monday' etc). If the input is invalid, print an error message.
+
+// const number = prompt('pick a number between 1-7')
+//     if(number == '1'){
+//         console.log('Monday')
+//     } else if (number == '2'){
+//         console.log('Tuesday')
+//     } else if (number == '3'){
+//         console.log('Wednesday')
+//     } else if (number == '4'){
+//         console.log('Thursday')
+//     } else if (number == '5'){
+//         console.log('Friday')
+//     } else if (number == '6'){
+//         console.log('Saturday')
+//     } else if (number == '7'){
+//         console.log('Sunday')
+//     }else {
+//         console.log(ERROR)
+//     }
+
+// Letter Grade
+// Write a script that asks for a score number between 0-100. Print the corresponding grade for that number. (i.e. <60 = F, <70 = D, <80 = C, <90 = B, 90+ = A). If the input is invalid, print an error message.
+
+// const number = prompt('what is your grade')
+// if(number < 60){
+//     console.log('F')
+// }else if(number >= "60" && number <'70'){
+//     console.log('D')
+// }else if(number >= "70" && number <'80'){
+//     console.log('C')
+// }else if(number >= "80" && number <'90'){
+//     console.log('B')
+// }else if(number >= "90"){
+//     console.log('A')
+// }else {
+//     console.log('error')
+// }
+
+// Picnic Game
+// Write a script that asks the user for their name. Ask them for the food they are bringing to the picnic. Check if two input strings start with the same letter. If it does, tell them they can come to the party. If it doesn't, tell them to try again.
+
+const name = prompt('what is your name and what food are you bringing to the picnic?')
+}
+
+// Username length validator
+// Write a script that accepts a parameter username, and checks if the username is valid. A valid username must be longer than 3 characters and less than 18.
+
+// Odd and Divisible by 3
+// Write a script that asks the user for a number, then checks if the input number is odd and divisible by 3.
+
+const number = promopt('what is your favorite number?')
+    if (number % 3 = 0){
+        console.log('true')
+    }
+
+// Uppercase or Lowercase?
+// Write a program that prompts the user for a letter and checks if it is uppercase or lowercase.
+
+
+
+// Convert Temperature
+// Write a program that prompts the user for a temperature in Fahrenheit and converts it to Celsius.
+
+const number = prompt('what is the tempature in Fahrenheit')
+if (number = C = 5/9 x (F-32)) {
+    console.log()
+}
